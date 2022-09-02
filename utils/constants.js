44976d2254b36018e44e5257adfb545ecd71b4b8
Melty <meltygd@gmail.com>
2022-09-02 09:24:04 +0300

    fix(cors): add cors allowList

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-const allowlist = ['http://localhost:3000'];
+const allowlist = ['http://localhost:3000', 'https://meltywd.github.io/test-project-rs-frontend'];
 
 const limiterSetting = {
   windowMs: 15 * 60 * 1000, // за 15 минут
